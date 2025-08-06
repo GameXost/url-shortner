0901048966305d3a1b18e454e424b455be7c480e
GameXost <dsergey010206@gmail.com>
2025-08-06 23:21:05 +0300

    :implemented middleware, logger, sqlite db

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -4,6 +4,12 @@ import (
 	"log/slog"
 	"os"
 	"url-shortner/internal/config"
+	"url-shortner/internal/http-server/middleware/logger"
+	"url-shortner/internal/lib/logger/sl"
+	"url-shortner/internal/storage/sqlite"
+
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
 )
 
 const (
@@ -18,6 +24,20 @@ func main() {
 	log = log.With(slog.String("env", cfg.Env))
 	log.Info("initializating server", slog.String("address", cfg.Address))
 	log.Debug("logger debug mode enabled")
+
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("failed to initialize storage", sl.Err(err))
+	}
+
+	router := chi.NewRouter()
+
+	router.Use(middleware.RequestID)
+	// router.Use(middleware.Logger)
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.URLFormat)
+	router.Use(logger.New(log))
+
 }
 
 func setupLogger(env string) *slog.Logger {
--- internal/storage/sqlite/sqlite.go
+++ internal/storage/sqlite/sqlite.go
@@ -2,7 +2,9 @@ package sqlite
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
+	"url-shortner/internal/storage"
 )
 
 type Storage struct {
@@ -31,4 +33,71 @@ func New(storagePath string) (*Storage, error) {
 		return nil, fmt.Errorf("%s: %w", op, err)
 	}
 	return &Storage{db: db}, nil
+}
+
+func (s *Storage) SaveURL(urlToSave string, alias string) (int64, error) {
+	const op = "storage.sqlite.SaveURL"
+
+	stmt, err := s.db.Prepare("INSERT INTO url(url, alias) values(?, ?)")
+	if err != nil {
+		return 0, fmt.Errorf("%s: prepare statement: %w", op, err)
+	}
+
+	res, err := stmt.Exec(urlToSave, alias)
+	if err != nil {
+		if sqliteErr, ok := err.(sqlite3.Error); ok && sqliteErr.ExtendedCode == sqlite3.ErrConstaintUnique {
+			return 0, fmt.Errorf("%s: %w", op, storage.ErrURLExists)
+		}
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	id, err := res.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("%s: failed to get last insert id: %w", op, err)
+	}
+	return id, nil
+}
+
+func (s *Storage) GetURL(alias string) (string, error) {
+	const op = "storage.sqlite.GetURL"
+
+	stmt, err := s.db.Prepare("SELECT url FROM url WHERE alias = ?")
+	if err != nil {
+		return "", fmt.Errorf("%s: prepare statement: %w", op, err)
+	}
+
+	var resURL string
+
+	err = stmt.QueryRow(alias).Scan(&resURL)
+	if errors.Is(err, sql.ErrNoRows) {
+		return "", storage.ErrURLNotFound
+	}
+
+	if err != nil {
+
+		return "", fmt.Errorf("%s: execute statement: %w", op, err)
+	}
+	return resURL, nil
+}
+
+// TODO: deleteURL func
+func DeleteURL(alias string) (string, error) {
+	const op = "storage.sqlite.DeletweURL"
+	stmt, err := s.db.Prepare("DELETE FROM url WHERE alias = ?")
+	if err != nil {
+		return "", fmt.Errorf("%s: statement: %w", op, err)
+	}
+	res, err := stmt.Exec(alias)
+	if err != nil {
+		return "", fmt.Errorf("%s: excecute statement: %w", op, err)
+	}
+	rowsAffected, err := res.RowsAffected()
+	if err != nil {
+		return "", fmt.Errorf("%s: get rows affected: %w", op, err)
+	}
+	if rowsAffected == 0 {
+		return "", fmt.Errorf("%s: %w", op, storage.ErrURLNotFound)
+	}
+	return "URL deleted", nil
+
 }
